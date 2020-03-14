292c2de64740903f751af9badb2776e25185eac4
jiindev <jiindev@gmail.com>
2020-03-14 23:59:42 +0900

    redux 기본 설정

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import Head from 'next/head';
+import propTypes from 'prop-types';
 import AppLayout from '../components/AppLayout';
+import withRedux from 'next-redux-wrapper';
+import {createStore, compose, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from '../reducers';
 
 
-
-const Kit = ({Component}) => {
+const Kit = ({Component, store}) => {
     return (
         <>
+        <Provider store={store}>
             <Head>
                 <title>Kiiten In The box!</title>
             </Head>
                 <AppLayout>
                     <Component/>
                 </AppLayout>
-            
+        </Provider>
         </>
     )
 }
 
-export default Kit
+Kit.propTypes = {
+    Component:propTypes.elementType,
+    store: propTypes.object
+};
+
+export default withRedux((initialState, options)=>{
+    const middlewares = [];
+    const enhancer = compose(applyMiddleware(...middlewares),
+        !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+    );
+    const store = createStore(reducer, initialState, enhancer);
+    return store;
+})(Kit);
+
